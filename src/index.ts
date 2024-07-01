1b335f52a05126c2e9516e4347d36bb913f4400a
Alexin <alexindevs@gmail.com>
2024-07-01 13:05:59 +0100

    add jsdoc

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,12 @@
 import axios from 'axios';
 
+/**
+ * Retrieves the location information for a given IP address.
+ *
+ * @param {string} ip - The IP address to fetch location information for.
+ * @return {Promise<any>} - A promise that resolves to the location information.
+ * @throws {Error} - If there is an error fetching the location information.
+ */
 export async function getIpLocation(ip: string) {
   try {
     const response = await axios.get(`http://ip-api.com/json/${ip}`);
